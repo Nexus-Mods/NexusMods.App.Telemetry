1981446babdd63d6efc6abad6c0f3c2c0703bfa1
erri120 <erri120@protonmail.com>
2023-08-17 10:10:19 +0200

    Add note

--- src/NexusMods.Telemetry/Metrics/Counters.cs
+++ src/NexusMods.Telemetry/Metrics/Counters.cs
@@ -45,6 +45,11 @@ public static class Counters
 
     private static string GetOperatingSystemInformation()
     {
+        // NOTE(erri120): Because Windows is just weird, there is no easy way
+        // to extract the release number, eg: "Windows 10" or "Windows 11".
+        // The best you can do is get the kernel version, which is super jank:
+        // https://learn.microsoft.com/en-us/dotnet/api/system.environment.osversion
+        // https://learn.microsoft.com/en-us/windows/win32/sysinfo/operating-system-version
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return "Windows";
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return "Linux";
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) return "macOS";
